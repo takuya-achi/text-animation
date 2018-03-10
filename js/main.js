ccda7fe13762fe7fec06c3f28f09d85c5cb70643
Takyuya Achi <atp92nesics@gmail.com>
2018-03-10 15:15:05 +0900

    text-03 and responsive complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-$('.section-2 .letters').each(function(){
+ $('.section-2 .letters').each(function(){
     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-  });
+});
+
+
 
   anime.timeline()
+
     .add({
         targets: '.fadeup .fadeup-wrapper .letter',
-        translateY: ["3em", 0],
+        translateY: ["4em", 0],
         translateX: ["0", 0],
         translateZ: 0,
         duration: 1000,
@@ -21,3 +24,26 @@ $('.section-2 .letters').each(function(){
         easing: "easeOutExpo",
         delay: 2000
     });
+
+
+    
+  anime.sideshow()
+
+     .add({
+        targets: '.sideshow .sideshow-wrapper .letter',
+        translateY: ["0", 0],
+        translateX: ["2em", 0],
+        opacity: [0, 1],
+        translateZ: 0,
+        duration: 2000,
+        delay: 2000,
+        easing: "easeOutExpo",
+        delay: function(el, i) {
+            return 400 * i;
+        }
+
+    }).add({
+        targets: '.sideshow .sideshow-wrapper', 
+    });
+
+    
